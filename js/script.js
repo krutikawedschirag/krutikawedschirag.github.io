34c58279418072a1acf8b94fedce9c12fa8aaede
darshitvvora <darshit@quetzal.in>
2018-04-30 15:17:08 +0530

    added slider

--- js/script.js
+++ js/script.js
@@ -112,7 +112,7 @@
 
 	
 	/*CountDown*/
-	$('.married_coundown').countdown({until: new Date(2018, 12, 05)});
+	$('.married_coundown').countdown({until: new Date(2015, 12, 19)});
 	
 	/*OWL Carousel in Our Story*/
 	$(".story_wrapper").owlCarousel({
